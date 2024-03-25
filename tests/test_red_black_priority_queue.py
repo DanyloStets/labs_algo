b5f2787adc77d012d9b587de800d154fbf006a67
DanyloStets <danylost2006@gmail.com>
2024-03-25 13:55:43 +0200

    add solution and tests for lab4
    variant 1, level 3

---
+++ tests/test_red_black_priority_queue.py
@@ -0,0 +1,21 @@
+import unittest
+import sys
+
+sys.path.append("D:\\PyCharm\\learingPy")
+from red_black_priority_queue import RedBlackTree
+
+class TestRedBlackPriorityQueue(unittest.TestCase):
+    def test_peek_sequence(self):
+        tree = RedBlackTree()
+        tree.insert(1, 11)
+        tree.insert(30, 10)
+        tree.insert(11, 6)
+        tree.insert(12, 7)
+        tree.insert(13, 3)
+        tree.insert(14, 1)
+        tree.insert(15, 8)
+        tree.insert(16, 4)
+        self.assertEqual(tree.delete(), 11)
+
+if __name__ == "__main__":
+    unittest.main()
